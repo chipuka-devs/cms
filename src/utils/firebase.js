e377f6ff05ae859c4797935e18e6ede25ae84215
pekstaar <pekstaar@gmail.com>
2022-04-22 18:28:01 +0300

    UPDATE:ui update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, Timestamp } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDxmgec7lMOvTAyTd2_vi8eFB1Na4VCu0A",
+//   authDomain: "choir-web-app.firebaseapp.com",
+//   projectId: "choir-web-app",
+//   storageBucket: "choir-web-app.appspot.com",
+//   messagingSenderId: "727169626617",
+//   appId: "1:727169626617:web:be25602a7d766d94074d28",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDxmgec7lMOvTAyTd2_vi8eFB1Na4VCu0A",
-  authDomain: "choir-web-app.firebaseapp.com",
-  projectId: "choir-web-app",
-  storageBucket: "choir-web-app.appspot.com",
-  messagingSenderId: "727169626617",
-  appId: "1:727169626617:web:be25602a7d766d94074d28",
+  apiKey: "AIzaSyAWDh1UUlPgUFLb0bQhOu-TfAUCIoHR5LQ",
+  authDomain: "app-contributions.firebaseapp.com",
+  projectId: "app-contributions",
+  storageBucket: "app-contributions.appspot.com",
+  messagingSenderId: "482592875267",
+  appId: "1:482592875267:web:4eeb0a2261256660483f36",
 };
 
 // Initialize Firebase
