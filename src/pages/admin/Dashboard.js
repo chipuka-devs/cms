505c71176a420ae662cdbfe3007c4b67e059ab9d
chipuka-devs <104217318+chipuka-devs@users.noreply.github.com>
2022-08-04 17:34:46 +0300

    [UPDATE]:chart titles

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -72,9 +72,11 @@ export const Dashboard = () => {
       {/* charts */}
       <div className="py-4 flex gap-2">
         <div className="w-7/12 rounded-lg bg-white p-2 min-h-[500px]">
+          <h3>Actual vs Budget Overview</h3>
           <Chart />
         </div>
         <div className="w-5/12 rounded-lg bg-white p-2">
+        <h3> Contribution Mix </h3>
           <PiChart state={state} colors={colors} />
         </div>
       </div>
